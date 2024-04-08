33a45179a01edd3b2a45c441be394debfa2df100
Andreyhuey <andreyhuey777@gmail.com>
2024-04-08 19:04:40 +0100

    add: Footer component

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ This is a FullStack Nextjs Event ticket application
 
 ## Tools
 
-[Shadcn](https://ui.shadcn.com/docs/installation/next) for UI library
+[Shadcn](https://ui.shadcn.com/docs/installation/next) a UI library for faster web development
 
 [Clerk](https://clerk.com) for authentication and complete user management
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <div>
-      <></>
-    </div>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex flex-between flex-col gap-4 p-5 text-center sm:flow-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/favicon.ico"
+            alt="logo"
+            width={38}
+            height={18}
+          />
+        </Link>
+        <p>2024 EventsMingle. All Rights reserved.</p>
+      </div>
+    </footer>
   );
 };
 
