cfdbde52200510652fee6c25025695b3f186a2c2
Andreyhuey <andreyhuey777@gmail.com>
2024-04-12 00:33:39 +0100

    add: category & order model, user actions, webhook routes

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,6 +1,8 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
-import { WebhookEvent } from "@clerk/nextjs/server";
+import { WebhookEvent, clerkClient } from "@clerk/nextjs/server";
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
@@ -52,23 +54,59 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
-
-if(eventType === "user.created"){
-    const {id, email_addresses, image_url, first_name, last_name, username} = evt.data
+  // if a user is created
+  if (eventType === "user.created") {
+    const { id, email_addresses, image_url, first_name, last_name, username } =
+      evt.data;
 
     const user = {
-        clerkId: id,
-        email: email_addresses[0].email_address,
-        username: username,
-        firstName: first_name,
-        lastName: last_name,
-        photo: image_url
-    }
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
+    };
 
     // A database action
-    // const newUser = await createUser(user)
-}
+    const newUser = await createUser(user);
 
+    if (newUser) {
+      await clerkClient.users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id,
+        },
+      });
+    }
+    return NextResponse.json({ message: "OK", user: newUser });
+  }
+
+  // if a user is updated
+  if (eventType === "user.updated") {
+    const { id, image_url, first_name, last_name, username } = evt.data;
+
+    const user = {
+      firstName: first_name,
+      lastName: last_name,
+      username: username!,
+      photo: image_url,
+    };
+
+    const updatedUser = await updateUser(id, user);
+
+    return NextResponse.json({ message: "OK", user: updatedUser });
+  }
+
+  // if a user is deleted
+  if (eventType === "user.deleted") {
+    const { id } = evt.data;
+
+    const deletedUser = await deleteUser(id!);
+
+    return NextResponse.json({ message: "OK", user: deletedUser });
+  }
+
+  return new Response("", { status: 200 });
 
   return new Response("", { status: 200 });
 }
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,12 +1,74 @@
-"use server"
+"use server";
 
-import { CreateUserParams } from "@/types"
-import { handleError } from "../utils"
+import { CreateUserParams, UpdateUserParams } from "@/types";
+import { handleError } from "../utils";
+import { connectToDatabase } from "../database";
+import User from "../database/models/user.model";
+import Event from "../database/models/event.model";
+import Order from "../database/models/order.model";
+import { revalidatePath } from "next/cache";
 
-export const createUser = async (user:CreateUserParams) => {
-    try{
-        
-    }catch (error) {
-        handleError(error)
+// server actions for creating a user
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    await connectToDatabase();
+    const newUser = await User.create(user);
+
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+// server actions for updating a user
+export async function updateUser(clerkId: string, user: UpdateUserParams) {
+  try {
+    await connectToDatabase();
+
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+      new: true,
+    });
+
+    if (!updatedUser) throw new Error("User update failed");
+    return JSON.parse(JSON.stringify(updatedUser));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// server actions for deleting a user
+export async function deleteUser(clerkId: string) {
+  try {
+    await connectToDatabase();
+
+    // Find user to delete
+    const userToDelete = await User.findOne({ clerkId });
+
+    if (!userToDelete) {
+      throw new Error("User not found");
     }
+
+    // Unlink relationships, smart removes all refrences to the user
+    await Promise.all([
+      // Update the 'events' collection to remove references to the user
+      Event.updateMany(
+        { _id: { $in: userToDelete.events } },
+        { $pull: { organizer: userToDelete._id } },
+      ),
+
+      // Update the 'orders' collection to remove references to the user
+      Order.updateMany(
+        { _id: { $in: userToDelete.orders } },
+        { $unset: { buyer: 1 } },
+      ),
+    ]);
+
+    // Delete user
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+    revalidatePath("/");
+
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
+  } catch (error) {
+    handleError(error);
+  }
 }
