d4a52563724aecdc30e19e01ab67308cbe7cddf9
Andreyhuey <andreyhuey777@gmail.com>
2024-04-23 00:20:44 +0100

    add: uploadthing styles added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import "@uploadthing/react/styles.css";
 
 const poppins = Poppins({
   subsets: ["latin"],
